9a60566c2398335500c075a6bb52d44d8618ed89
flaxodotdev <321779115+flaxodotdev@users.noreply.github.com>
2026-09-01 21:12:53 +0300

    build: strip release binaries

--- build.zig
+++ build.zig
@@ -70,6 +70,7 @@ pub fn build(b: *std.Build) void {
             // definition if desireable (e.g. firmware for embedded devices).
             .target = target,
             .optimize = optimize,
+            .strip = optimize != .Debug,
             // List of modules available for import in source files part of the
             // root module.
             .imports = &.{
